10dc77bb2c7a2fa2686307c7f59a16398af5b336
Troy Furlow <tsf85@humboldt.edu>
2021-02-01 13:42:19 -0800

    Learning my way around scraping/requesting. Encountered a problem where when getting text for the boardgame names, the list it prints is finding the correct number of game names, but prints only the first game name the correct amount of times...

---
+++ main.py
@@ -0,0 +1,39 @@
+#import urllib
+import requests
+from bs4 import BeautifulSoup
+#import urllib.request, urllib.parse, urllib.error
+from csv import writer
+#import datetime
+#import os
+#from os.path import isfile, join
+#import csv
+#import shutil
+#import sqlite3
+
+vasel_range_one = (10)
+vasel_range_two = (1)
+vasel_top_index = ('{}-{}'.format(vasel_range_one, vasel_range_two))
+
+vasel_url = ('https://www.dicetower.com/game-video/tom-vasels-top-100-games-all-time-2021-{}'.format(vasel_top_index))
+quinn_url = ('https://www.shutupandsitdown.com/videos/quinns-walks-you-through-his-game-collection/')
+
+response1 = requests.get(vasel_url)
+#html1 = response1.read()
+soup1 = BeautifulSoup(response1.text, "html.parser")
+
+games = soup1.find_all(class_='game_thumbnail')
+
+for gt_title in games:
+    titles = soup1.find(class_='gt_title').get_text().replace('\n', '')
+    print(titles)
+    
+
+
+    
+       
+
+
+
+    
+
+ 
