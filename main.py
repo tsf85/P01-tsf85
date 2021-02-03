b74a9b72b60333e3235a145928e0a2134c3e3e32
Troy Furlow <tsf85@humboldt.edu>
2021-02-02 16:34:13 -0800

    Tom vasel's top 100 list is broken up into ten boardgames per page. Made a for loop to pull the ten from each page.

--- main.py
+++ main.py
@@ -10,29 +10,32 @@ import csv
 #import shutil
 #import sqlite3
 
-vasel_range_one = (10)
-vasel_range_two = (1)
-vasel_top_index = ('{}-{}'.format(vasel_range_one, vasel_range_two))
 
-vasel_url = ('https://www.dicetower.com/game-video/tom-vasels-top-100-games-all-time-2021-{}'.format(vasel_top_index))
+
 quinn_url = ('https://www.shutupandsitdown.com/videos/quinns-walks-you-through-his-game-collection/')
 
-response1 = requests.get(vasel_url)
-#html1 = response1.read()
-soup1 = BeautifulSoup(response1.text, "html.parser")
-
-titles = soup1.find_all(class_='gt_title')
-
-for div in titles:
-    game = soup1.find('a').get_text().replace('\n', '')
-print(game)
-
-
-    
-       
 
 
 
-    
+
+def get_vasel_top_list():
+    i = 0
+   
+    for i in range (91):
+        vasel_range_one = (i+10)
+        vasel_range_two = (i+1)
+        vasel_top_index = ('{}-{}'.format(vasel_range_one, vasel_range_two))
+        vasel_url = ('https://www.dicetower.com/game-video/tom-vasels-top-100-games-all-time-2021-{}'.format(vasel_top_index))
+        response1 = requests.get(vasel_url)
+        src1 = response1.content
+        soup1 = BeautifulSoup(src1, 'html.parser')
+        links = soup1.find_all(class_='gt_title')
+        for link in links:
+            if "" in link.text:
+                print(link.text)
+                i += 10
+                get_vasel_top_list
+
+get_vasel_top_list()
 
  
