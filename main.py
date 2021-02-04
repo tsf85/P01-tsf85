f3a8cf2ca3311c88710c796d97d533db8d71bb2d
Troy Furlow <tsf85@humboldt.edu>
2021-02-04 12:20:07 -0800

    Added the scrape for Quinn's top list, from ShutUpAndSitDown. Couldn't figure out how to exclude a couple of irrelevant bit of text from being scraped, but it shouldn't immediately matter since the objective is to merge top boardgames in common.
    should have made these separate commits, but I added the file writing and a clearing function to each scrape.
    
    sample lists attached.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #import urllib
+import pathlib
 import requests
 from bs4 import BeautifulSoup
 #import urllib.request, urllib.parse, urllib.error
@@ -12,11 +13,11 @@ import csv
 
 
 
-quinn_url = ('https://www.shutupandsitdown.com/videos/quinns-walks-you-through-his-game-collection/')
 
 
-
-
+def clear_vasel_list():
+    file = open("Vasel_list.txt", "w")
+    file.close()
 
 def get_vasel_top_list():
     i = 0
@@ -32,10 +33,36 @@ def get_vasel_top_list():
         links = soup1.find_all(class_='gt_title')
         for link in links:
             if "" in link.text:
-                print(link.text)
+                file = open("Vasel_list.txt", "a") 
+                file.write(str(link.text))
+                file.write('\n')
+                file.close()
+                #print(link.text)
                 i += 10
                 get_vasel_top_list
 
-get_vasel_top_list()
 
+get_vasel_top_list()
+#clear_vasel_list()
+
+def clear_quinn_list():
+    file = open("Quinn_list.txt", "w")
+    file.close()
+
+def get_quinn_top_list():
+    quinn_url = ('https://www.shutupandsitdown.com/videos/quinns-walks-you-through-his-game-collection/')
+    response2 = requests.get(quinn_url)
+    src2 = response2.content
+    soup2 = BeautifulSoup(src2, 'html.parser')
+    links = soup2.find_all('strong')
+    for link in links:
+        if "" in link.text:
+            file = open("Quinn_list.txt", "a") 
+            file.write(str(link.text))
+            file.write('\n')
+            file.close()
+            #print(link.text)
+
+#get_quinn_top_list()
+#clear_quinn_list()
  
